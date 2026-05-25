7090e31cc3934fe004ef3749ad5e37bd56c308dd
1ardafn1-glitch <1ardafn1@gmail.com>
2026-05-25 19:16:55 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    socket.on("message", (msg) => {
+        io.emit("message", msg);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server çalışıyor");
+});
