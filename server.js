600495050f7763b90b16bcf8e3b687a90984318c
1ardafn1-glitch <1ardafn1@gmail.com>
2026-05-25 19:31:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,31 +6,32 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// 📁 frontend dosyaları
 app.use(express.static("public"));
 
-// 💬 bağlantı
 io.on("connection", (socket) => {
 
-    console.log("Bir kullanıcı bağlandı");
+    console.log("Kullanıcı bağlandı");
 
-    // 💬 mesaj alma ve herkese gönderme
+    // 💬 mesaj
     socket.on("message", (data) => {
         io.emit("message", {
-            name: data.name || "Anonim",
+            name: data.name,
             msg: data.msg
         });
     });
 
-    // ❌ bağlantı kopma
+    // ✍️ yazıyor
+    socket.on("typing", (name) => {
+        socket.broadcast.emit("typing", name);
+    });
+
     socket.on("disconnect", () => {
-        console.log("Bir kullanıcı çıktı");
+        console.log("Kullanıcı çıktı");
     });
 });
 
-// ⚡ Railway PORT (ÇOK ÖNEMLİ)
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log("Server çalışıyor port:", PORT);
+    console.log("Server çalışıyor:", PORT);
 });
